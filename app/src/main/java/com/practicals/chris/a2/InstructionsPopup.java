84bd2b1fe4c06a374830268172d7d2cff99e0399
chris-breed <christopher.breed@my.jcu.edu.au>
2018-05-18 16:58:02 +1000

    instruction popup sizer up

--- app/src/main/java/com/practicals/chris/a2/InstructionsPopup.java
+++ app/src/main/java/com/practicals/chris/a2/InstructionsPopup.java
@@ -17,7 +17,7 @@ public class InstructionsPopup extends Activity {
         int width = displayMetrics.widthPixels;
         int height = displayMetrics.heightPixels;
 
-        getWindow().setLayout((int) (width * 0.9), (int) (height * 0.9));
+        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.8));
 
     }
 }
